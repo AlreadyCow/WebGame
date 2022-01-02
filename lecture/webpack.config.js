8c8c59c357662398cd98589aa2162f1556edb0a7
AlreadyCow <0828miso@gmail.com>
2022-01-02 16:43:20 +0900

    4강_반응속도체크

--- lecture/webpack.config.js
+++ lecture/webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     },
 
     entry : {
-        app : ['./client2', './client3'],
+        app : ['./client2', './client3', './client4'],
     }, // 입력
     module : {
         rules : [{
