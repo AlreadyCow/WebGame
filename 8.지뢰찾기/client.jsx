c780709f540787841fcaafa5eb9614262e1597bb
AlreadyCow <0828miso@gmail.com>
2022-01-09 20:16:28 +0900

    8강. 지뢰찾기

--- 8.지뢰찾기/client.jsx
+++ 8.지뢰찾기/client.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDom from "react-dom";
 //import { hot } from 'react-hot-loader/root';
 
-import TicTacToe from "./MineSearch";
+import MineSearch from "./MineSearch";
 
 // const Hot = hot(MineSearch);
 // ReactDom.render(<Hot/>, document.querySelector('#root'));
